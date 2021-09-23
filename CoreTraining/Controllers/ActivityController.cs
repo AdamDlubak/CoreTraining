94f4fe46d329e28e4a99b9e00db3340b83e0d341
Adam Dłubak <adam.dlubak@gmail.com>
2021-09-23 12:53:07 +0200

    Connected with Azure SQL Database + Added .gitignore

---
+++ CoreTraining/Controllers/ActivityController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using CoreTraining.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreTraining.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+    public ActivityController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public IEnumerable<Activity> Get()
+        {
+            var activities = new List<Activity>();
+
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("HubDatabase")))
+            {
+                const string sql = "SELECT Id, UniqueReferenceCode, CreationTime, QuotingRentMinId, QuotingRentMaxId FROM Activity";
+
+                connection.Open();
+                using SqlCommand command = new SqlCommand(sql, connection);
+                using SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var activity = new Activity()
+                    {
+                        Id = new Guid(reader["Id"].ToString() ?? string.Empty),
+                        CreationTime = DateTimeOffset.Parse(reader["CreationTime"].ToString() ?? string.Empty),
+                        QuotingRentMinId = new Guid(reader["QuotingRentMinId"].ToString() ?? string.Empty),
+                        QuotingRentMaxId = new Guid(reader["QuotingRentMaxId"].ToString() ?? string.Empty),
+                    };
+
+                    activities.Add(activity);
+                }
+            }
+
+            return activities;
+        }
+    }
+}
---
+++ CoreTraining/Models/Activity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTraining.Models
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public string UniqueReferenceCode { get; set; }
+        public DateTimeOffset CreationTime { get; set; }
+        public Guid ActivityStatusId { get; set; }
+        public Guid? QuotingRentMinId { get; set; }
+        public Guid? QuotingRentMaxId { get; set; }
+    }
+}
